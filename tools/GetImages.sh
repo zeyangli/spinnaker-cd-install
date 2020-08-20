30e2d650da2baef177f020655976983faa9c0c83
lizeyang <610556220@qq.com>
2020-08-20 19:25:51 +0800

    Create GetImages.sh

---
+++ tools/GetImages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+S_REGISTRY="gcr.io/spinnaker-marketplace"
+T_REGISTRY="registry.cn-beijing.aliyuncs.com/spinnaker-cd"
+NODES="node01.zy.com node02.zy.com"
+
+## 下载镜像
+function GetImages(){
+    echo -e "\033[43;34m =====GetImg===== \033[0m"
+
+    IMAGES=$( cat tagfile.txt)
+
+    for image in ${IMAGES}
+    do
+        for node in ${NODES}
+        do 
+           echo  -e "\033[32m ${node} ---> pull ---> ${image} \033[0m"
+           ssh ${node} "docker pull ${T_REGISTRY}/${image}"
+           echo  -e "\033[32m ${node} ---> tag ---> ${image} \033[0m"
+           ssh ${node} "docker tag ${T_REGISTRY}/${image} ${S_REGISTRY}/${image}"
+        done
+    done
+    for node in ${NODES}
+    do
+       echo -e "\033[43;34m =====${node}===镜像信息===== \033[0m"
+       ssh ${node} "docker images | grep 'spinnaker-marketplace' "
+    done
+    
+}
+
+GetImages
