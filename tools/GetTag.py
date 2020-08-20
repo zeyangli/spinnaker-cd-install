fc8ada1271c69fe70484333ab2e581ccef8aab53
lizeyang <610556220@qq.com>
2020-08-20 16:56:42 +0800

    Create GetTag.py

---
+++ tools/GetTag.py
@@ -0,0 +1,30 @@
+import yaml
+import sys
+import os 
+
+filePath = sys.argv[1]
+tagFile = sys.argv[2]
+
+
+print("======>Get Yaml Data<=======")
+file = open(filePath, 'r', encoding="utf-8")
+fileData = file.read()
+file.close()
+
+print("======>Yaml To Dict<=======")
+data = yaml.load(fileData)
+print(data['services'])
+
+print("======> Service Name <======")
+serviceData = data['services']
+print(serviceData.keys())
+
+print("======> Get Tag <======")
+os.system("rm -fr " + tagFile)
+for s in serviceData.keys():
+    if s != "defaultArtifact":
+      print(s + ":" + serviceData[s]['version'])
+      tag = s + ":" + serviceData[s]['version']
+      f = open(tagFile, 'a')
+      f.write(tag + "\n")
+      f.close()
